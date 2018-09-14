08cf0bc114e3e31ee45c9336d841f56888a8293a
Garet McKinley <self@garet.io>
2018-09-14 18:43:28 -0500

    initial commit

---
+++ src/gatsby-node.js
@@ -0,0 +1,12 @@
+// const readingTime = require("reading-time");
+
+exports.onCreateNode = ({ node, actions }) => {
+  const { createNodeField } = actions;
+  if (node.internal.type === `MarkdownRemark`) {
+    createNodeField({
+      node,
+      name: `readingTime`,
+      value: "test",
+    });
+  }
+};
